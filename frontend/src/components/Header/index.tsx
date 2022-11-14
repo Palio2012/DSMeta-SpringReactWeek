ada7f2d52f63a6635640f15bc52eaf065a87825e
Palio2012 <daniasrayan.com@gmail.com>
2022-11-14 12:43:47 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <>
+             <header>
+        <div className="dsmeta-logo-container">
+            <img src= {logo} alt="DSMeta" />
+            <h1>DSMeta</h1>
+            <p>
+              Desenvolvido por
+              <a href="https://github.com/Palio2012">@Palio2012</a>
+            </p>
+        </div>
+    </header>
+        </>
+    )
+
+}
+
+export default Header
