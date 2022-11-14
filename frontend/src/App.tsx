dd354d20b651c53de984dc6de2ac457677b7e2c4
Palio2012 <daniasrayan.com@gmail.com>
2022-11-14 11:54:48 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
-
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 
 }
