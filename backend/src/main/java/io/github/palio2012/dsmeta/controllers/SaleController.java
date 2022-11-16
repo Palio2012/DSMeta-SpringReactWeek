0ad87d78441197a0455cb9f13a70c24f43fd6c94
Palio2012 <daniasrayan.com@gmail.com>
2022-11-16 13:13:19 -0300

    Twilio SMS system implementation with environment variables (application.properties)

--- backend/src/main/java/io/github/palio2012/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/io/github/palio2012/dsmeta/controllers/SaleController.java
@@ -2,13 +2,11 @@ package io.github.palio2012.dsmeta.controllers;
 
 import io.github.palio2012.dsmeta.entities.Sale;
 import io.github.palio2012.dsmeta.services.SaleService;
+import io.github.palio2012.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -17,6 +15,8 @@ public class SaleController {
 
     @Autowired
     private SaleService service;
+    @Autowired
+    private SmsService smsService;
 
     @GetMapping
     public Page <Sale> findSales (
@@ -24,6 +24,11 @@ public class SaleController {
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms (@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
 
