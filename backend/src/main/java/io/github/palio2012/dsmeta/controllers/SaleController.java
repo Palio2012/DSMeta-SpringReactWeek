6408876e3b0fbabcffb2edf202e2b79d404c2c74
Palio2012 <daniasrayan.com@gmail.com>
2022-11-16 11:29:48 -0300

    Date select filter implemented, (minDate, maxDate time window)

--- backend/src/main/java/io/github/palio2012/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/io/github/palio2012/dsmeta/controllers/SaleController.java
@@ -3,11 +3,13 @@ package io.github.palio2012.dsmeta.controllers;
 import io.github.palio2012.dsmeta.entities.Sale;
 import io.github.palio2012.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,8 +19,11 @@ public class SaleController {
     private SaleService service;
 
     @GetMapping
-    public List<Sale> findSales () {
-        return service.findSales();
+    public Page <Sale> findSales (
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return service.findSales(minDate, maxDate, pageable);
     }
 }
 
--- backend/src/main/java/io/github/palio2012/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/io/github/palio2012/dsmeta/repositories/SaleRepository.java
@@ -1,11 +1,18 @@
 package io.github.palio2012.dsmeta.repositories;
 
 import io.github.palio2012.dsmeta.entities.Sale;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
 
 @Repository
 public interface SaleRepository extends JpaRepository <Sale, Long>{
 
 
+    @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+    Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
