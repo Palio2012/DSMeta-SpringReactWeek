c4efea2d18f8b290d6721c5f313c3626282da9e0
Palio2012 <daniasrayan.com@gmail.com>
2022-11-18 12:53:09 -0300

    Toast lib notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -3,6 +3,7 @@ import icon from '../../assets/img/notification-icon.svg';
 import './styles.css';
 import axios from 'axios';
 import { BASE_URL } from '../../utils/request';
+import { toast } from 'react-toastify';
 
 type Props = {
     saleId: number;
@@ -11,7 +12,7 @@ type Props = {
 function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-        console.log("SUCESSO")
+        toast.info("SMS enviado com sucesso");
     })
 }
 
